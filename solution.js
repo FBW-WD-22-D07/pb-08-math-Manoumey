0aa8f01917b07a20133c69b796d741b6a904988d
Manoumey <manuella.meyer@dci.education>
2023-01-03 15:59:38 +0100

    first commit

---
+++ solution.js
@@ -0,0 +1,33 @@
+
+//console.log(Math.random());
+//console.log("---------");
+//console.log(Math.floor(Math.random() *10));
+//console.log(Math.random() *100);
+
+//1.Minimum und Maximum
+
+//a. Niedrigste Zahl
+console.log(Math.min(-1, 4));
+
+//b. Höchste Zahl
+console.log(Math.max(-1, 4));
+
+
+//2.Runden
+//a.Aufrunden
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
+
+//b.Abrunden
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+
+//3. Würfelwurf!
+
+console.log(Math.floor(Math.random() *6 +1));
